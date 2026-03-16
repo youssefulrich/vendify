252cd96cd7275cd1c4c0a7331dddcdf1d3559f2e
Ulrich Koffi <koffiulrich538@gmail.com>
2026-03-16 12:54:15 +0000

    fix: session definitive

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,6 +4,13 @@ import { Database } from './types'
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+      }
+    }
   )
 }
