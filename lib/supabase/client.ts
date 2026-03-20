50360e4d15b4605abcda9db539f312b128d63a6a
Ulrich Koffi <koffiulrich538@gmail.com>
2026-03-20 15:41:45 +0000

    fix: clients cookie

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -7,10 +7,16 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       auth: {
-        persistSession: true,
+        persistSession:   true,
         autoRefreshToken: true,
         detectSessionInUrl: true,
-      }
+      },
+      cookieOptions: {
+        maxAge: 60 * 60 * 24 * 30, // 30 jours
+        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        path: '/',
+      },
     }
   )
 }
